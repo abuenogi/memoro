9d3a77a9ee4ea7b012ab909347f51eceedb55301
memoro-react <memoro.react@gmail.com>
2020-04-29 18:30:50 +0200

    primer  commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import "./index.css";
 
-
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
+import ChangePassword from "./components/ChangePassword";
 
 
 ReactDOM.render(
 
     <BrowserRouter >
-        <App />
+        <Router>
+            <div className="auth-inner">
+                <Switch>
+                    <Route exact path='/' component={Login} />
+                    <Route path="/sign-in" component={Login} />
+                    <Route path="/sign-up" component={SignUp} />
+                    <Route path="/change-password" component={ChangePassword} />
+                </Switch>
+            </div>
+
+        </Router>
     </BrowserRouter>,
     document.getElementById("root")
 );
