18c28d0bd0a7500fb6472cc58a03faa3897cec4e
AB <memoro.react@gmail.com>
2020-08-07 18:56:41 +0200

    AB: se añade en CampoMapa el botón Cancelar

--- src/components/CampoMapa.js
+++ src/components/CampoMapa.js
@@ -95,7 +95,7 @@ const CampoMapa = ({ history}) => {
                 )}
 
             </Map>
-            <Button type="submit" className="btn-block mt-5 button1"
+            <Button type="submit" className="btn-block mt-2 button1"
                 onClick={e => { 
 
                     //const modalContainer = document.querySelector("#modal-root"); 
@@ -109,6 +109,11 @@ const CampoMapa = ({ history}) => {
                         casa: ubiCasa
                       }) }}
             > Volver</Button>
+            <Button type="submit" className="btn-block mt-2 button1"
+                onClick={e => {                    
+                    document.querySelector("#modal-root").style.display = 'none';
+                     }}
+            > Cancelar</Button>
         </Fragment>
     )
 
